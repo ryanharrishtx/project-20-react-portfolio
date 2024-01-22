d6e7c20960499378638e316582f83af9d4e136de
ryanharrishtx@gmail.com <ryanharrishtx@gmail.com>
2024-01-22 13:57:55 -0600

    working on Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
+import React from "react";
+import Header from "./components/Header";
+// import Footer from "./components/Footer";
 
 function App() {
 
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        <Navbar />
-      </h1>
+      <Header />
     </div>
   )
 }
